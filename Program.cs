b0895fc0ca79220e04b89bdeae8d10d0ae55c9c1
Gøran Smistad <goran@smistad.org>
2020-09-21 09:06:15 +0200

    call move on each animal

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace Zoology
             {
                 animal.describe();
                 animal.say();
+                animal.move();
                 Console.WriteLine();
             });
         }
